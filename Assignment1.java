59d6c8a78f5faf743b2715a3e2d64b630bc1a995
Isaac Jones <idjones@ilstu.edu>
2020-01-27 20:49:28 -0600

    Changed to ALL CAPS

--- Assignment1.java
+++ Assignment1.java
@@ -1,39 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package assignment1;
+run:
+Please enter text: test
+The length of your text is: 4
 
-import java.util.*;
-/**
- *
- * @author Isaac
- */
-public class Assignment1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {        
-        System.out.print("Please enter text: ");
-        Scanner scan = new Scanner(System.in);
-	String input = scan.nextLine();
-        
-        String output = "";
-        
-        System.out.print("The length of your text is: " + input.length() + "\n");
-        
-        System.out.print("\nYour text with every other letter capitalized: ");
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if (i % 2 == 0) {
-                output += input.substring(i, i + 1).toUpperCase();
-            } else {
-                output += input.substring(i, i + 1);
-            }
-        }
-        System.out.println(output);
-    }
-}
+Your text with every other letter capitalized: TeSt
+BUILD SUCCESSFUL (total time: 2 seconds)
